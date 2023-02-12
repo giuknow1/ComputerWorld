fee810b554a4b6cc66e9824d07163c0ac6932a2c
giuknow1 <giulianomacchiavelli03@gmail.com>
2023-02-12 18:03:28 -0300

    Modificado Script Camara para que la camara nu se mueva cuando el inventario este abierto

--- Assets/Scripts/Personaje/Camara.cs
+++ Assets/Scripts/Personaje/Camara.cs
@@ -7,21 +7,50 @@ public class Camara : MonoBehaviour
     public float sensibilidad = 5.0f; 
     public float suavizado = 2.0f; 
     
+    public bool control_camara = false;
+    public bool controlQ = false;
+    
     GameObject jugador; 
     
+
+    public void ActivacionControl()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && controlQ == false)
+        {
+            control_camara = !control_camara;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            controlQ = true;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            controlQ = false;
+        }
+    }
+
     void Start() 
-    { 
+    {
+       
         jugador = this.transform.parent.gameObject; 
     } 
     
     void Update() 
-    { 
-        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        md = Vector2.Scale(md, new Vector2(sensibilidad * suavizado, sensibilidad * suavizado));
-        suavidad.x = Mathf.Lerp(suavidad.x, md.x, 1f / suavizado); 
-        suavidad.y = Mathf.Lerp(suavidad.y, md.y, 1f / suavizado); 
-        Mira += suavidad; Mira.y = Mathf.Clamp(Mira.y, -90f, 90f); 
-        transform.localRotation = Quaternion.AngleAxis(-Mira.y, Vector3.right);
-        jugador.transform.localRotation = Quaternion.AngleAxis(Mira.x, jugador.transform.up); 
+    {
+        ActivacionControl();
+
+        if (control_camara == false)
+        {
+            var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            md = Vector2.Scale(md, new Vector2(sensibilidad * suavizado, sensibilidad * suavizado));
+            suavidad.x = Mathf.Lerp(suavidad.x, md.x, 1f / suavizado);
+            suavidad.y = Mathf.Lerp(suavidad.y, md.y, 1f / suavizado);
+            Mira += suavidad; Mira.y = Mathf.Clamp(Mira.y, -90f, 90f);
+            transform.localRotation = Quaternion.AngleAxis(-Mira.y, Vector3.right);
+            jugador.transform.localRotation = Quaternion.AngleAxis(Mira.x, jugador.transform.up);
+        }
     } 
 }
